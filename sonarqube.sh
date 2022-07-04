e3620dcf6ae8ca15ca2bc6333903a1bfd672d840
rameshagwd <rameshc.gwd@gmail.com>
2022-07-04 11:56:38 +0200

    modifyed sq file

--- sonarqube.sh
+++ sonarqube.sh
@@ -1,7 +1,37 @@
 #!/bin/bash
+LOG=/tmp/stack.log 
 
 echo "Installing SonarQube Dependences"
-yum install wget unzip java -y
+yum install wget unzip java -y &>>LOG
 
 echo "Downloading Mysql Package"
-wget https://repo.mysql.com/yum/mysql-connectors-community/el/7/x86_64/mysql-community-release-el7-5.noarch.rpm
+cd /tmp
+wget https://repo.mysql.com/yum/mysql-connectors-community/el/7/x86_64/mysql-community-release-el7-5.noarch.rpm &>>LOG
+
+echo "Installing Mysql"
+rpm -ivh mysql-community-release-el7-5.noarch.rpm
+yum install mysql-server -y
+
+echo "Starting Mysql Service"
+systemctl start mysqld
+
+echo "Updating Mysql Config"
+echo "CREATE DATABASE sonarqube_db;
+CREATE USER 'sonarqube_user'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON sonarqube_db.* TO 'sonarqube_user'@'localhost' IDENTIFIED BY 'password';
+FLUSH PRIVILEGES;" > /tmp/sonar.sql
+mysql < /tmp/sonar.sql
+
+echo "Creating User for SonarQube DB"
+usage() {
+  printf "create-user [-u|--username] <sonarqube>\n"
+  printf " OPTIONS\n"
+  printf "  -u --username\sonarqube of the new account (required)\n"
+  printf "  -p --password\sonar@123 for the new account (optional)\n"
+  printf "  -h --help\tprint this help\n"
+  exit 1
+}
+
+echo "Downloading SonarQube Package"
+cd /tmp
+wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-6.7.6.zip
