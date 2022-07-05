31e29b69ed5da0cc7efd9742115a68e56ead033d
rameshagwd <rameshc.gwd@gmail.com>
2022-07-05 18:03:24 +0200

    Updated the SQ file

--- sonarqube04.sh
+++ sonarqube04.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+ID=$(id -u)
 LOG=/tmp/stack.log
 ##### "This is applying Regular expression with cut command "#############
 MYSQL_PKG=https://repo.mysql.com/yum/mysql-connectors-community/el/7/x86_64/mysql-community-release-el7-5.noarch.rpm
@@ -8,9 +9,17 @@ MYSQL_RPM=$(echo $MYSQL_PKG | cut -d/ -f9)
 SQ_PKG=https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-6.7.6.zip
 SQ_ZIP=$(echo $SQ_PKG awk -F / '{print $NF}')
 
+if [$ID -ne 0]; then
+    echo "You do not have the admin privileges to run this script file.......!"
+    exit1
+else
+    echo "Running the script file with admin privileges.......!"
+fi
+########Appling Funcations#############
 VALIDATE() {
     if [ $1 -ne 0 ]; then
         echo "$2 ... FAILED"
+        exit 1
     else
         echo "$2 ... SUCCESS"
     fi
@@ -62,9 +71,10 @@ if [ -f /tmp/$MYSQL_RPM ]; then
             mv sonarqube-6.7.6 /opt/sonarqube
             chown -R sonarqube. /opt/sonarqube
             VALIDATE $? "Downloading/Extracting/Installing SonarQube Package"
+
+            echo 'sonar.jdbc.username=sonarqube_user
+            sonar.jdbc.password=password
+            sonar.jdbc.url=jdbc:mysql://localhost:3306/sonarqube_db?useUnicode=true&amp;characterEncoding=utf8&amp;rewriteBatchedStatements=true&amp;useConfigs=maxPerformance' >> /opt/sonarqube/conf/sonar.properties
+            VALIDATE $? "Updating SonarQube DB Details"
 fi
 
-echo 'sonar.jdbc.username=sonarqube_user
-sonar.jdbc.password=password
-sonar.jdbc.url=jdbc:mysql://localhost:3306/sonarqube_db?useUnicode=true&amp;characterEncoding=utf8&amp;rewriteBatchedStatements=true&amp;useConfigs=maxPerformance' >> /opt/sonarqube/conf/sonar.properties
-VALIDATE $? "Updating SonarQube DB Details"
